a8845a274f8c4c777f5cc9f128f3f897a67ee5b5
Sia819 <jjn970819@gmail.com>
2020-12-04 05:22:30 +0900

    upload source

---
+++ PrintRefresh/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Windows.Forms;
+
+namespace PrintRefresh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stop the spooler.
+            ServiceController service = new ServiceController("Spooler");
+            if ((!service.Status.Equals(ServiceControllerStatus.Stopped)) &&
+                (!service.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped);
+            }
+
+            // Start the spooler.
+            service.Start();
+            service.WaitForStatus(ServiceControllerStatus.Running);
+
+            MessageBox.Show("프린터 새로고침 완료.");
+        }
+    }
+}
